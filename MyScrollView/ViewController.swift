ca174971d14fa32fcfaf5056f2af2e6b2e15ccc7
Dima Protsak <protsack.dima25@gmail.com>
2023-04-06 14:10:06 +0300

    newTypeScrollView

--- MyScrollView/ViewController.swift
+++ MyScrollView/ViewController.swift
@@ -8,41 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private let swipeableView: UIView = {
-            let view = UIView(frame: CGRect(origin: .zero,
-                                            size: CGSize(width: 400.0, height: 300.0)))
-            view.backgroundColor = .blue
-            view.translatesAutoresizingMaskIntoConstraints = false
+    private var customScrollView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        view.translatesAutoresizingMaskIntoConstraints = false
 
-            return view
+        return view
         }()
+    
+    private let subview1: UIView = {
+        let view = UIView(frame: CGRect(origin: .init(x: 50, y: 300),
+                                        size: CGSize(width: 100.0, height: 100.0)))
+        view.backgroundColor = .green
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
+    
+    private let subview2: UIView = {
+        let view = UIView(frame: CGRect(origin: .init(x: 250, y: 200),
+                                        size: CGSize(width: 100.0, height: 100.0)))
+        view.backgroundColor = .green
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(swipeableView)
-
-        view.backgroundColor = .white
+        view.addSubview(customScrollView)
+        customScrollView.addSubview(subview1)
+        customScrollView.addSubview(subview2)
+        customScrollView.frame = view.bounds
         
-        swipeableView.addGestureRecognizer(createSwipeGestureRecognizer(for: .up))
-        swipeableView.addGestureRecognizer(createSwipeGestureRecognizer(for: .down))
+        
+        customScrollView.addGestureRecognizer(createSwipeGestureRecognizer(for: .down))
+        customScrollView.addGestureRecognizer(createSwipeGestureRecognizer(for: .up))
 }
     
+    
     @objc private func didSwipe(_ sender: UISwipeGestureRecognizer) {
-
-        var frame = swipeableView.frame
-
-        switch sender.direction {
-        case .up:
-            frame.origin.y -= 100.0
-        case .down:
-            frame.origin.y += 100.0
-        default:
-            break
-        }
-
-        UIView.animate(withDuration: 0.25) {
-            self.swipeableView.frame = frame
+        
+        for i in 0..<customScrollView.subviews.count {
+            let subview = customScrollView.subviews[i]
+            var frame = subview.frame
+            
+            switch sender.direction {
+            case .up:
+                frame.origin.y -= 100.0
+            case .down:
+                frame.origin.y += 100.0
+            default:
+                break
+            }
+            
+            
+            UIView.animate(withDuration: 0.25) {
+                subview.frame = frame
+            }
         }
     }
     
